9acff665f61bb4f8e8efa5560ff3b75382c44c3a
Lukas Elflein <lukaselflein@gmail.com>
2020-04-25 09:11:48 +0200

    cleanup

--- bin/test.py
+++ bin/test.py
@@ -1,21 +1,22 @@
 from trainapp import *
 
 lu_acc = Account(name='lu')
-print(lu_acc)
-
 lu_trainer = lu_acc.add_trainer_role()
 lu_player = lu_acc.add_player_role()
+
 r_path = '../data/example_running.csv'
-lu_trainer.load_plan(r_path, name = 'testname')
+lu_trainer.load_plan(r_path, name = 'running')
 s_path = '../data/example_strength.csv'
-lu_trainer.load_plan(s_path, name = 'testname')
+lu_trainer.load_plan(s_path, name = 'strength')
 
-plan = lu_trainer.plans.pop()
-lu_trainer.assign_plan(player=lu_player, plan=plan)
+for plan in lu_trainer.plans:
+    lu_trainer.assign_plan(player=lu_player, plan=plan)
 
 
-print(lu_player)
-print(lu_player.plans)
-lu_player.claim_training(plan)
+lu_player.show_plans()
+for plan in lu_player.plans:
+    print(plan)
+    lu_player.claim_training(plan)
+
 print()
 print(lu_player)

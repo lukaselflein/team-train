de6582e77cbbce0395a4fdd8210b1346183b2ede
Lukas Elflein <lukaselflein@gmail.com>
2020-05-12 19:07:31 +0200

    add team & roles to json web token

--- backend/database/models.py
+++ backend/database/models.py
@@ -27,12 +27,21 @@ class User(db.Document):
     email = db.EmailField(required=True, unique=True)
     password = db.StringField(required=True, min_length=6)
     workouts = db.ListField(db.ReferenceField('Workout', reverse_delete_rule=db.PULL))
+    roles = db.ListField()
+    team = db.StringField()
 
     def hash_password(self):
         self.password = generate_password_hash(self.password).decode('utf8')
 
     def check_password(self, password):
         return check_password_hash(self.password, password)
+
+    def set_team(self, team):
+        self.team = team
+
+    def add_role(self, role):
+        if role not in self.roles:
+            self.roles += [role]
 
 
 User.register_delete_rule(Workout, 'added_by', db.CASCADE)

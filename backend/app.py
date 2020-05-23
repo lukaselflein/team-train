e093bf66f03a4248b006c24efa830516495a94ec
Lukas Elflein <lukaselflein@gmail.com>
2020-05-23 12:24:46 +0200

    add debugging

--- backend/app.py
+++ backend/app.py
@@ -20,4 +20,4 @@ initialize_db(app)
 initialize_routes(api)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
--- backend/run.sh
+++ backend/run.sh
@@ -1,5 +1,6 @@
 #export FLASK_ENV=development
 export ENV_FILE_LOCATION=./.env
+export PYTHONDONTWRITEBYTECODE=1 
 
 python3 -m unittest tests/test_signup.py 
 python3 -m unittest tests/test_login.py 

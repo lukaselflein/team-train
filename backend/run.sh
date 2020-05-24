f40a185f82608174f475619a7aa5d37463bc807b
Lukas Elflein <lukaselflein@gmail.com>
2020-05-24 08:44:56 +0200

    add logger for inspecting requests

--- backend/run.sh
+++ backend/run.sh
@@ -2,8 +2,8 @@
 export ENV_FILE_LOCATION=./.env
 export PYTHONDONTWRITEBYTECODE=1 
 
-python3 -m unittest tests/test_signup.py 
-python3 -m unittest tests/test_login.py 
+#python3 -m unittest tests/test_signup.py 
+#python3 -m unittest tests/test_login.py 
 python3 -m unittest tests/test_create_workout.py 
 
 echo 'starting server...'

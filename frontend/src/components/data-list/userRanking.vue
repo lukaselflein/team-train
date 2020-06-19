31f723b369b1c3bd486cf9921b924892540ab7a3
Olga Maus <s70861@beuth-hochschule.de>
2020-06-19 15:55:26 +0200

    memberList and user ranking

---
+++ frontend/src/components/data-list/userRanking.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <b-tab title="Ranking" active>
+      <TeamTable class="tab" />
+    </b-tab>
+    <!-- <b-card-footer>(dummy data)</b-card-footer> -->
+  </div>
+</template>
+
+<script>
+import TeamTable from "@/components/data-detail/teamTable.vue";
+
+export default {
+  name: "UserRanking",
+  components: {
+    TeamTable
+  }
+};
+</script>
+
+<style scoped>
+.tab {
+  margin-top: 1em;
+}
+</style>

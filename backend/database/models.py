7af9a7149edf175164cc4f0374dcc48a684ec2f3
Lukas Elflein <lukaselflein@gmail.com>
2020-10-04 15:46:17 +0200

    add join/leave team functionality, move GET whoami request into GET user

--- backend/database/models.py
+++ backend/database/models.py
@@ -66,14 +66,42 @@ class Workout(db.Document):
         return workout_dict
 
 
-class Role(db.Document, RoleMixin):
+class Role(db.Document):
+    """Abstract role object for controlling user permissions."""
     name = db.StringField(max_length=80, unique=True)
     description = db.StringField(max_length=255)
 
 
-class Team(db.Document, RoleMixin):
+class RoleContainer(db.Document):
+    """Container object for user roles."""
+    roles = db.ListField(db.ReferenceField('Role'), default=[])
+
+
+class Team(db.Document):#, RoleMixin):
     name = db.StringField(max_length=80, unique=True)
     description = db.StringField(max_length=255)
+    # TODO: add reference to all members in team
+    members = None
+    team_score = None
+
+    def delete(self):
+        """Remove team object; kick all members from team."""
+        print('Team deletion triggered. Not implemened.')
+        # TODO
+
+    def show_score(self):
+        """Returns sum of scores of team members."""
+        score = 0
+        for member in self.members:
+            score += member.score
+        return score
+
+    def show_member_names(self):
+        """Returns list of member-name strings."""
+        return [member.name for member in self.members]
+
+    def __str__(self):
+        return self.name
 
 
 class User(db.Document):
@@ -81,7 +109,6 @@ class User(db.Document):
     username = db.StringField(required=True, unique=True)
     password = db.StringField(required=True, min_length=6)
     workouts = db.ListField(db.ReferenceField('Workout', reverse_delete_rule=db.PULL))
-    roles = db.ListField(db.ReferenceField('Role'), default=[])
     team = db.ReferenceField('Team', default=None)
     current_score = db.IntField(default=0)
     score_history = db.ListField(db.ReferenceField('ScoreLog', reverse_delete_rule=db.PULL))
@@ -94,6 +121,7 @@ class User(db.Document):
         return check_password_hash(self.password, password)
 
     def add_manager_role(self):
+        """User is now manager, can modify teams"""
         try:
             manager_role = Role.objects.get(name='manager')
         except:
@@ -107,6 +135,7 @@ class User(db.Document):
             raise RuntimeError('Role already assigned')
 
     def rm_manager_role(self):
+        """User no longer is manager"""
         try:
             manager_role = Role.objects.get(name='manager')
         except:
@@ -115,8 +144,47 @@ class User(db.Document):
 
         if manager_role in self.roles:
             self.update(pull__roles=manager_role)
+            self.save()
         else:
             raise RuntimeError('Cannot remove, role not assigned')
 
+    def join_team(self, team_name):
+        """Join existing team; Error on missing rights or missing team"""
+        try:
+            team = Role.objects.get(name=team_name)
+        except:
+            raise RuntimeError('Team does not exist.')
 
+        if self.team:
+            raise RuntimeError('User already in team.')
+
+        self.team = team
+        self.save()
+
+    def leave_team(self):
+        """Leave current team"""
+        if self.team is None:
+            raise RuntimeError('User not in team, cannot leave.')
+
+        # TODO: if user is last member of team, delete it
+        # if len(self.team.members) == 1:
+        #       self.team.delete()
+        #       return 'team deleted'
+
+        self.team = None
+        self.save()
+
+        # TODO: if user is last member with manager role, transfer role to last active member
+        
+
+    def create_team(self, team_name):
+        """Create new team and join"""
+        # TODO: Check if team exists
+        # Error if if does exist
+        # Create team otherwise
+        # join team
+        pass
+
+
+# Remove all workouts created by user upon deletion of user object
 User.register_delete_rule(Workout, 'added_by', db.CASCADE)
--- backend/resources/roles.py
+++ backend/resources/roles.py
@@ -1,115 +1,120 @@
 import json
-from functools import wraps
 from flask import Response, request
 from database.models import Workout, User, Role, Team
 from flask_restful import Resource
 from flask_jwt_extended import jwt_required, get_jwt_identity
 
-from mongoengine.errors import FieldDoesNotExist, \
-NotUniqueError, DoesNotExist, ValidationError, InvalidQueryError
-
-from resources.errors import SchemaValidationError, WorkoutAlreadyExistsError, \
-InternalServerError, UpdatingWorkoutError, DeletingWorkoutError, WorkoutNotExistsError, \
-UnauthorizedError
-
-def default_exception_handler(function):
-    """Handle form exceptions."""
-    def wrap(*args, **kwargs):
-        try:
-            return function(*args, **kwargs)
-        except TypeError:
-            raise SchemaValidationError
-        except InvalidQueryError:
-            raise SchemaValidationError
-        except Exception:
-            raise InternalServerError
-   
-    return wrap
-
-def manager_role_required(function):
-    """Allow only user with mangager role."""
-    @wraps(function)
-    def wrap(*args, **kwargs):
-        user = User.objects.get(id=get_jwt_identit())
-        if 'manager' not in [role.name for role in user.roles]:
-            raise UnauthorizedError
-        return function(*args, **kwargs)
-   
-    return wrap
+from resources.decorators import default_exception_handler, manager_role_required, coach_role_required, check_same_team
 
 
-def coach_role_required(function):
-    """Allow only user with coach role."""
-    @wraps(function)
-    def wrap(*args, **kwargs):
-        user = User.objects.get(id=get_jwt_identit())
-        if 'coach' not in [role.name for role in user.roles]:
-            raise UnauthorizedError
-
-        return function(*args, **kwargs)
-   
-    return wrap
-
-
-def check_same_team(source_user, target_user):
-    """Check if actor and target are in same team."""
-
-    # If active user is not in team, deny
-    if source_user.team is None:
-        raise UnauthorizedError
-    
-    # If users are in team, it must be the same
-    if source_user.team != target_user.team:
-        raise UnauthorizedError
-
-    return True
-
-
-def manager_role_required(function):
-    @wraps(function)
-    def wrap(*args, **kwargs):
-        # First, check if user is manager for the team
-        user_id = get_jwt_identity()
-        user = User.objects.get(id=user_id)
-        print(user.team)
-        if 'manager' in [role.name for role in user.roles]:
-            print('user is manager')
-        else:
-            raise UnauthorizedError
-        return function(*args, **kwargs)
-   
-    return wrap
-
+# TODO
+# Length restriction on username, teamname, etc
 
 class TeamApi(Resource):
-  @default_exception_handler
-  @jwt_required
-  def post(self):
-    user_id = get_jwt_identity()
-    user = User.objects.get(id=user_id)
-    response_data = json.dumps({'username': user.username,
-                                'email': user.email,
-                                'roles': [role.name for role in user.roles],
-                                'team': user.team.name})
-    return Response(response_data, mimetype="application/json", status=200)
+    """Manage teams and their members."""
+    @jwt_required
+    @default_exception_handler
+    def post(self):
+        """User creates a new team and join it"""
+        active_user = User.objects.get(id=get_jwt_identity())
+        if active_user.team is not None:
+            return f'User must leave team before creating a new team', 412
+
+        body = request.get_json()
+        requested_teamname = body['name']
+
+        # Check for name collision
+        try:
+            Team.objects.get(name=requested_teamname)
+            return f'Teamname {requested_teamname} already exists', 412
+        except:
+            pass
+
+        team = Team(**body)
+        team.save()
+
+        active_user.team = team
+        active_user.save()
+
+        return f'Requested team {requested_teamname} created.', 200
+
+    @jwt_required
+    @default_exception_handler
+    @manager_role_required
+    def delete(self):
+        '''Manager deletes a team and leaves it'''
+        active_user = User.objects.get(id=get_jwt_identity())
+        if active_user.team is None:
+            return f'User must be in team to delete it.', 400
+
+        requested_teamname = request.get_json()['name']
+
+        # Check if team exists
+        if not Team.objects.get(name=requested_teamname): 
+            return f'Teamname {requested_teamname} does not exist', 400
 
 
-class WhoamiApi(Resource):
-  @default_exception_handler
-  @jwt_required
-  def get(self):
-    user_id = get_jwt_identity()
-    user = User.objects.get(id=user_id)
-    response_data = json.dumps({'username': user.username,
-                                'email': user.email,
-                                'roles': [role.name for role in user.roles],
-                                'team': user.team.name})
-    return Response(response_data, mimetype="application/json", status=200)
+        target_team = Team.objects.get(name=requested_teamname)
+        target_team.delete()
+        active_user.leave_team()
+
+        return f'Requested team {requested_teamname} deleted.', 200
+
+
+class UserApi(Resource):
+   """Modify the user account."""
+
+   @jwt_required
+   @default_exception_handler
+   def get(self):
+      """Info on current user."""
+      user_id = get_jwt_identity()
+      user = User.objects.get(id=user_id)
+      response_data = json.dumps({'username': user.username,
+                                  'email': user.email,
+                                  'roles': [role.name for role in user.roles],
+                                  'team': str(user.team)})
+      return Response(response_data, mimetype="application/json", status=200)
+
+   @jwt_required
+   @default_exception_handler
+   def post(self):
+       '''Join a team.'''
+       user = User.objects.get(id=get_jwt_identity())
+       if user.team is not None:
+           return f'User must not be in a team to join a team.', 400
+
+       requested_teamname = request.get_json()['name']
+       user.join_team(name=requested_teamname)
+
+       return f'User {user.name} left team.', 200
+
+   @jwt_required
+   @default_exception_handler
+   def put(self):
+       '''Leave a team.'''
+       user = User.objects.get(id=get_jwt_identity())
+        
+       body = request.get_json()
+       requested_teamname = body['name']
+
+       if user.team is None:
+           return f'User must be in team to leave it.', 400
+
+       user.leave_team()
+       return f'User {user.name} left team.', 200
+
+   @jwt_required
+   @default_exception_handler
+   def delete(self):
+       """Delete user account"""
+       return f'User {user.name} deleted - NOT IMPLEMENTED', 200
 
 
 class WhoisApi(Resource):
-  @default_exception_handler
+  """Print info on target user."""
   @jwt_required
+  @default_exception_handler
   def get(self):
     body = request.get_json()
     username = body['username']
@@ -122,8 +127,9 @@ class WhoisApi(Resource):
 
 
 class ManagerRoleApi(Resource):
-  @default_exception_handler
+  """Team-Manager admin role."""
   @jwt_required
+  @default_exception_handler
   #@manager_role_required
   def post(self):
     active_user = User.objects.get(id=get_jwt_identity())
@@ -135,8 +141,8 @@ class ManagerRoleApi(Resource):
     return f'Manager role added to {target_username}.', 200
 
 
-  @default_exception_handler
   @jwt_required
+  @default_exception_handler
   @manager_role_required
   def delete(self):
     active_user = User.objects.get(id=get_jwt_identity())
@@ -148,51 +154,3 @@ class ManagerRoleApi(Resource):
         return f'Manager role removed from {target_username}.', 200
 
 
-class TeamApi(Resource):
-  @default_exception_handler
-  @jwt_required
-  def post(self):
-    '''Create a new team and join it'''
-    active_user = User.objects.get(id=get_jwt_identity())
-    if active_user.team is not None:
-        return f'User must leave team before creating a new team', 412
-
-    body = request.get_json()
-    requested_teamname = body['name']
-
-    # Check for name collision
-    if Team.objects.get(name=requested_teamname): 
-        return f'Teamname {requested_teamname} already exists', 412
-
-    team = Team(**body)
-    team.save()
-
-    active_user.team = team
-    active_user.save()
-
-    return f'Requested team {requested_teamname} created.', 200
-
-
-  @default_exception_handler
-  @jwt_required
-  def put(self):
-    '''Delete a team and join it'''
-    active_user = User.objects.get(id=get_jwt_identity())
-    if active_user.team is not None:
-        active_user.team = None
-        active_user.save()
-
-    body = request.get_json()
-    requested_teamname = body['name']
-
-    # Check for name collision
-    if not Team.objects.get(name=requested_teamname): 
-        return f'Teamname {requested_teamname} already exists', 412
-
-    team = Team(**body)
-    team.save()
-
-    active_user.team = team
-    active_user.save()
-
-    return f'Requested team {requested_teamname} created.', 200
